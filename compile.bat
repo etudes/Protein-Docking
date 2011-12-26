0bcee7702d391853492dbef511e8996bc954f888
Vikram Sundar <14vikrams@students.harker.org>
2011-12-25 19:31:03 -0800

    Added compile.bat

---
+++ compile.bat
@@ -0,0 +1,2 @@
+rmdir /s /q org
+javac -d . src\java\org\vikramdock\ProteinDockPredict.java src\java\org\vikramdock\Atom.java src\java\org\vikramdock\ProteinStruct.java src\java\org\vikramdock\TestCase.java src\java\org\vikramdock\Constants.java
