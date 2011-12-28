b794a0d9ed0bcfe491566f78a7e3c3fd4916d9ca
Vikram Sundar <14vikrams@students.harker.org>
2011-12-27 16:13:07 -0800

    Added bond stretching

---
+++ src/java/org/vikramdock/Bond.java
@@ -0,0 +1,35 @@
+package org.vikramdock;
+
+import java.io.*;
+import java.util.*;
+import java.util.zip.*;
+import java.lang.*;
+import java.lang.reflect.*;
+
+public class Bond {
+	Atom first;
+	Atom second;
+	double equildist;
+	double k;
+	public Bond(Atom first, Atom second, double k) {
+		this.first = first;
+		this.second = second;
+		equildist = first.distance(second);
+		this.k = k;
+	}
+	public Atom getFirst() {
+		return first;
+	}
+	public Atom getSecond() {
+		return second;
+	}
+	public double getEquilDist() {
+		return equildist;
+	}
+	public double getK() {
+		return k;
+	}
+	public double bondE(double realdist) {
+		return 1/2 * k * Math.pow(realdist - equildist, 2);
+	}
+}
